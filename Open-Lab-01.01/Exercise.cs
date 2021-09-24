5c458b44f7d0817a6b88c34e0d7d10dc8636dd5c
janosheen <triggered71@gmail.com>
2021-09-24 09:35:52 +0200

    I Parsed input string to int and returned the state.

--- Open-Lab-01.01/Exercise.cs
+++ Open-Lab-01.01/Exercise.cs
@@ -6,7 +6,16 @@ namespace Open_Lab_01._01
     {
         public bool IsLessThan10(string input)
         {
-            throw new NotImplementedException();
+            int res;
+            Int32.TryParse(input, out res);
+            if (res < 10)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
